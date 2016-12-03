68e1b2b91ce2c83e9e47e19928e9508b47a7012f
dk12de <daniel.kruse@danielyuko.de>
2016-12-03 20:27:18 +0100

    Small change

--- CSHash/Digests/CRC32.cs
+++ CSHash/Digests/CRC32.cs
@@ -16,17 +16,21 @@ namespace CSHash.Digests
 
         public byte[] HashFromByteArray(byte[] bArr)
         {
+            using (CSHash.Implementations.CRC32 crc = new CSHash.Implementations.CRC32())
             {
+                byte[] rawHash = crc.ComputeHash(bArr);
                 return rawHash;
             }
         }
 
         public async Task<byte[]> AsyncHashFromByteArray(byte[] bArr)
         {
+            using (CSHash.Implementations.CRC32 crc = new CSHash.Implementations.CRC32())
             {
                 byte[] bReturnHash = null;
                 await Task.Run(() =>
                 {
+                    byte[] rawHash = crc.ComputeHash(bArr);
                     bReturnHash = rawHash;
                 });
                 return bReturnHash;
@@ -35,17 +39,21 @@ namespace CSHash.Digests
 
         public byte[] HashFromString(string value)
         {
+            using (CSHash.Implementations.CRC32 crc = new CSHash.Implementations.CRC32())
             {
+                byte[] rawHash = crc.ComputeHash(Encoding.Default.GetBytes(value));
                 return rawHash;
             }
         }
 
         public async Task<byte[]> AsyncHashFromString(string value)
         {
+            using (CSHash.Implementations.CRC32 crc = new CSHash.Implementations.CRC32())
             {
                 byte[] bReturnHash = null;
                 await Task.Run(() =>
                     {
+                        byte[] rawHash = crc.ComputeHash(Encoding.Default.GetBytes(value));
                         bReturnHash = rawHash;
                     });
                 return bReturnHash;
@@ -54,10 +62,12 @@ namespace CSHash.Digests
 
         public byte[] HashFromFile(string filePath, int bufferSize = 12000000)
         {
+            using (CSHash.Implementations.CRC32 crc = new CSHash.Implementations.CRC32())
             {
                 byte[] bReturnHash = null;
                 using (BufferedStream bufferedStream = new BufferedStream(File.OpenRead(filePath), bufferSize))
                 {
+                    byte[] rawHash = crc.ComputeHash(bufferedStream);
                     bReturnHash = rawHash;
                 }
                 return bReturnHash;
@@ -66,12 +76,14 @@ namespace CSHash.Digests
 
         public async Task<byte[]> AsyncHashFromFile(string filePath, int bufferSize = 12000000)
         {
+            using (CSHash.Implementations.CRC32 crc = new CSHash.Implementations.CRC32())
             {
                 byte[] bReturnHash = null;
                 using (BufferedStream bufferedStream = new BufferedStream(File.OpenRead(filePath), bufferSize))
                 {
                     await Task.Run(() =>
                         {
+                            byte[] rawHash = crc.ComputeHash(bufferedStream);
                             bReturnHash = rawHash;
                         });
                 }
