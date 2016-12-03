2183807aa314b454d7bd03b70a8b42c837bbe3a6
dk12de <daniel.kruse@danielyuko.de>
2016-12-03 18:09:45 +0100

    First commit

---
+++ CSHash/Digests/MD5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHash
+{
+    public class MD5
+    {
+        public MD5()
+        {
+
+        }
+
+        public void HashFromByteArray(byte[] bArr)
+        {
+            
+        }
+    }
+}
