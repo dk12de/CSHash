c0d63b03f93c9a4569271667b54055cb33264427
dk12de <daniel.kruse@danielyuko.de>
2016-12-03 18:14:13 +0100

    Working on MD5 class

--- CSHash/Digests/MD5.cs
+++ CSHash/Digests/MD5.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace CSHash
 {
@@ -13,9 +15,27 @@ namespace CSHash
 
         }
 
-        public void HashFromByteArray(byte[] bArr)
+        public byte[] HashFromByteArray(byte[] bArr)
         {
-            
+            using (MD5Cng md5 = new MD5Cng())
+            {
+                byte[] rawHash = md5.ComputeHash(bArr);
+                return rawHash;
+            }
+        }
+
+        public async Task<byte[]> HashFromByteArray(byte[] bArr)
+        {
+            using (MD5Cng md5 = new MD5Cng())
+            {
+                byte[] bReturnHash = null;
+                await Task.Run(() =>
+                {
+                    byte[] rawHash = md5.ComputeHash(bArr);
+                    bReturnHash = rawHash;
+                });
+                return bReturnHash;
+            }
         }
     }
 }
